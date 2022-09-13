850da0e08983420ad36e0e917ffdafe35ad18c83
Lauren Ramgattie <lramgattie@twilio.com>
2022-09-13 13:11:14 -0400

    envs

--- airtable-js/airtableToJSON.js
+++ airtable-js/airtableToJSON.js
@@ -1,8 +1,7 @@
 require("dotenv").config()
 
 // Get Airtable Base
-console.log("Base ID")
-console.log(process.env.AIRTABLE_BASE_ID)
+console.log("Base ID" + process.env.AIRTABLE_BASE_ID)
 /*const Airtable = require("airtable")
 const base = new Airtable({ apiKey: process.env.AIRTABLE_KEY }).base(
   process.env.AIRTABLE_BASE_ID
