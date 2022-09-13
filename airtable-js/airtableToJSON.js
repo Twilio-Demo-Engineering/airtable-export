99718909b9e0107ba7b059c520121155e85cc011
Lauren Ramgattie <lramgattie@twilio.com>
2022-09-13 11:22:45 -0400

    node js

---
+++ airtable-js/airtableToJSON.js
@@ -0,0 +1,13 @@
+// Get Airtable Base
+const Airtable = require("airtable")
+const base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base(
+  process.env.AIRTABLE_BASE_ID
+)
+
+console.log(base)
+
+// Get Airtable Table
+
+// Convert to JSON
+
+// Output to JSON file
